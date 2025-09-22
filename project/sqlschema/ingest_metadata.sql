bccc15336b13765e45fc1760ea515dd7716be7ee
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-09-22 11:00:37 -0700

    regenerate files

--- project/sqlschema/ingest_metadata.sql
+++ project/sqlschema/ingest_metadata.sql
@@ -1,10 +1,11 @@
--- # Class: IngestMetadataFile Description: Information about a particular ingest perfomred to produce a KGX graph, describing source data, ingest process, and details of the resulting KGX graph.
+-- # Class: IngestMetadataFile Description: Information about a particular ingest performed to produce a KGX graph, describing source data, ingest process, and details of the resulting KGX graph.
 --     * Slot: id
 --     * Slot: file_name Description: An informative, human-readable name for this metadata file/object. e.g. "2025-08-18 Translator CTD Ingest Metadata"
 --     * Slot: file_creation_date Description: When this metadata file was created.
 --     * Slot: ingest_code_url Description: URL of the specific official release or tagged branch of the ingest code executed to perform the ingest.
+--     * Slot: ingest_code_version Description: The version of the ingest code executed to perform the ingest.
 --     * Slot: source_infores_id Description: Infores identifier of the ingested source.
---     * Slot: source_data_version Description: Version of the source data ingested (using source's own conventions).
+--     * Slot: source_data_version Description: Version of the source data ingested - using source's own conventions, or make one up  (e.g. use the date) if not provided by source.
 --     * Slot: source_access_date Description: Date the source data was accessed/downloaded into the system that performed the ingest.
 --     * Slot: target_name Description: A unique human readable name of the target data set/graph produced by this ingest. e.g. "2025-08-18 Translator CTD Ingest Graph"
 --     * Slot: target_creation_date Description: Date the target data set/graph was created.
@@ -14,7 +15,7 @@
 --     * Slot: target_data_model_version Description: Version of the target data model used to structure the KGX graph data. e.g. "4.2.6-rc5"
 --     * Slot: node_normalizer Description: Name/identifier of the algorithm/tool used to perform node normalization on the ingested data. e.g. "Babel"
 --     * Slot: node_normalizer_version Description: Version of the node normalization tool/algorithm.
---     * Slot: node_normalizer_url Description: URL(s) pointing to source code and/or information about the node normaliztion tool used. e.g.  "https://github.com/TranslatorSRI/NodeNormalization"
+--     * Slot: node_normalizer_url Description: URL(s) pointing to source code and/or information about the node normalization tool used. e.g.  "https://github.com/TranslatorSRI/NodeNormalization"
 --     * Slot: total_edge_count Description: Count of total edges in the graph.
 --     * Slot: total_node_count Description: Count of total nodes in the graph.
 --     * Slot: orphan_node_count Description: Count of nodes in the graph that do not participate in an edge.
@@ -39,8 +40,9 @@ CREATE TABLE "IngestMetadataFile" (
 	file_name TEXT NOT NULL,
 	file_creation_date DATE NOT NULL,
 	ingest_code_url TEXT NOT NULL,
+	ingest_code_version TEXT NOT NULL,
 	source_infores_id TEXT NOT NULL,
-	source_data_version TEXT,
+	source_data_version TEXT NOT NULL,
 	source_access_date DATE NOT NULL,
 	target_name TEXT NOT NULL,
 	target_creation_date DATE NOT NULL,
@@ -67,7 +69,7 @@ CREATE TABLE "IngestMetadataFile_source_access_urls" (
 	source_access_urls TEXT,
 	PRIMARY KEY ("IngestMetadataFile_id", source_access_urls),
 	FOREIGN KEY("IngestMetadataFile_id") REFERENCES "IngestMetadataFile" (id)
-);CREATE INDEX "ix_IngestMetadataFile_source_access_urls_source_access_urls" ON "IngestMetadataFile_source_access_urls" (source_access_urls);CREATE INDEX "ix_IngestMetadataFile_source_access_urls_IngestMetadataFile_id" ON "IngestMetadataFile_source_access_urls" ("IngestMetadataFile_id");
+);CREATE INDEX "ix_IngestMetadataFile_source_access_urls_IngestMetadataFile_id" ON "IngestMetadataFile_source_access_urls" ("IngestMetadataFile_id");CREATE INDEX "ix_IngestMetadataFile_source_access_urls_source_access_urls" ON "IngestMetadataFile_source_access_urls" (source_access_urls);
 CREATE TABLE "IngestMetadataFile_source_file_names" (
 	"IngestMetadataFile_id" INTEGER,
 	source_file_names TEXT,
@@ -79,7 +81,7 @@ CREATE TABLE "IngestMetadataFile_node_categories" (
 	node_categories TEXT,
 	PRIMARY KEY ("IngestMetadataFile_id", node_categories),
 	FOREIGN KEY("IngestMetadataFile_id") REFERENCES "IngestMetadataFile" (id)
-);CREATE INDEX "ix_IngestMetadataFile_node_categories_node_categories" ON "IngestMetadataFile_node_categories" (node_categories);CREATE INDEX "ix_IngestMetadataFile_node_categories_IngestMetadataFile_id" ON "IngestMetadataFile_node_categories" ("IngestMetadataFile_id");
+);CREATE INDEX "ix_IngestMetadataFile_node_categories_IngestMetadataFile_id" ON "IngestMetadataFile_node_categories" ("IngestMetadataFile_id");CREATE INDEX "ix_IngestMetadataFile_node_categories_node_categories" ON "IngestMetadataFile_node_categories" (node_categories);
 CREATE TABLE "IngestMetadataFile_edge_predicates" (
 	"IngestMetadataFile_id" INTEGER,
 	edge_predicates TEXT,
